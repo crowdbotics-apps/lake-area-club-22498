7ca5853fa65693fb3076359f730d6c8e229b527b
Crowdbotics <team@crowdbotics.com>
2020-11-10 23:34:46 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/lake_area_club_22498/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lake_area_club_22498;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "lake_area_club_22498";
+    }
+}
---
+++ ios/lake_area_club_22498Tests/lake_area_club_22498Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface lake_area_club_22498 : XCTestCase
+
+@end
+
+@implementation lake_area_club_22498
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
